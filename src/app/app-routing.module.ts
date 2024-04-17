bfdec90b7df5f02fb4f20e883017df8fec46bb0c
LeonardoCto <coutinholeonardo94@gmail.com>
2024-04-16 23:30:22 -0300

    commit sidebar click setores redirect to appcomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
+import { Setor } from './shared/model/Setor';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SetorDetalheComponent } from './setores/setor-detalhe/setor-detalhe.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'setores', pathMatch: 'full' },
   {
     path: 'setores',
     loadChildren:() => import('./setores/setores.module').then(m => m.SetoresModule)
-  }
+  },
+  {
+    path: 'setor-detalhe', component: SetorDetalheComponent }
+
 ];
 
 @NgModule({
